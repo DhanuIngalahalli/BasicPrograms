9ab248bbb87eae67e613b6e9c920490ec08147ef
sonupooja <ingalahallipooja@gmail.com>
2021-09-05 18:13:48 +0530

    checking number is even or odd

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 
-namespace Csharp
+namespace EvenorOdd
 {
     class Program
     {
-        static void Main(string[] args)
+
+
+        class MyApplication
         {
-            Console.WriteLine("Basic c sharp programs");
+            static void Main(string[] args)
+            {
+                int num1, num2, num3;
+                // set the value of the three numbers
+                num1 = 10;
+                num2 = 20;
+                num3 = 50;
+                if (num1 > num2)
+                {
+                    if (num1 > num3)
+                    {
+                        Console.Write("Number one is the largest!\n");
+                    }
+                    else
+                    {
+                        Console.Write("Number three is the largest!\n");
+                    }
+                }
+                else if (num2 > num3)
+                    Console.Write("Number two is the largest!\n");
+                else
+                    Console.Write("Number three is the largest!\n");
+            }
         }
     }
 }
