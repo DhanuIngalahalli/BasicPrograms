e7ae53043d232ff9041f54ce8f867e442e5831b2
sonupooja <ingalahallipooja@gmail.com>
2021-09-05 18:53:10 +0530

    checking power two of a number

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -1,23 +1,17 @@
 ﻿using System;
 
-namespace Leap_Year
+namespace Power_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int check_year;
-            Console.Write("Input an year : ");
-            check_year = Convert.ToInt32(Console.ReadLine());
-
-            if ((check_year % 400) == 0)
-                Console.WriteLine("{0} is a leap year.\n", check_year);
-            else if ((check_year % 100) == 0)
-                Console.WriteLine("{0} is not a leap year.\n", check_year);
-            else if ((check_year % 4) == 0)
-                Console.WriteLine("{0} is a leap year.\n", check_year);
-            else
-                Console.WriteLine("{0} is not a leap year.\n", check_year);
+            Console.WriteLine("enter the power");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= num; i++)
+            {
+                Console.WriteLine("2 to the power of {0} is" + Math.Pow(2, i), i);
+            }
         }
     }
 }
