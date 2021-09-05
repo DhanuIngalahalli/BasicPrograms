b8c10171616a7aefb5ca5b6cc372f93cbe51064b
sonupooja <ingalahallipooja@gmail.com>
2021-09-05 18:39:59 +0530

    harmonic of a number

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -1,51 +1,22 @@
 ﻿using System;
 
-namespace FlipCoin_Percentage
+namespace Harmonic_Number
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //flip the coin and calculate percentage
-            Console.WriteLine("Calculate the Percentage");
-            Random rand = new Random();
-            int heads = 0, tails = 0, result = 0;
+            Console.WriteLine("Harmonic Number Program");
+            double Num, harmonic = 0;
+            Console.Write("Enter Number For Harmonic sereis no. :");
+            Num = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 1; i <= 5; i++)
+            for (double i = 1; i <= Num; i++)
             {
-                result = rand.Next(0, 2);
-
-                if (result == 1)
-
-                {
-
-                    heads++;
-
-                }
-
-                else
-
-                {
-
-                    tails++;
-
-                }
-
-                double coin = rand.NextDouble();
-                if (coin < 0.5)
-                {
-                    Console.WriteLine(coin + " Tail");
-                    heads += 1;
-                }
-                else
-                {
-                    Console.WriteLine(coin + " Head");
-                    tails += 1;
-                }
+                harmonic = harmonic + (1 / i);
             }
-            Console.WriteLine("The Ratio of Tail:Head is : ({0}:{1})", heads, tails);
-            Console.WriteLine("Percentage of Tail is : {0}%", tails * 10);
-            Console.WriteLine("Percentage of Head is : {0}%", heads * 10);
+            Console.WriteLine("Harmonic Number of Number {0} is : {1}", Num, harmonic);
         }
     }
 }
+
