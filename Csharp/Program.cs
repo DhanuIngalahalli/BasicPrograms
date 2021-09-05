27d18c0952ab981602e09b83babc653e0ea3189d
sonupooja <ingalahallipooja@gmail.com>
2021-09-05 18:31:53 +0530

    percentage of flipcoin

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -1,21 +1,51 @@
 ﻿using System;
 
-namespace factor
+namespace FlipCoin_Percentage
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int a, b;
-            Console.WriteLine("Please enter your integer: ");
-            a = int.Parse(Console.ReadLine());
-            for (b = 1; b <= a; b++)
+            //flip the coin and calculate percentage
+            Console.WriteLine("Calculate the Percentage");
+            Random rand = new Random();
+            int heads = 0, tails = 0, result = 0;
+
+            for (int i = 1; i <= 5; i++)
             {
-                if (a % b == 0)
+                result = rand.Next(0, 2);
+
+                if (result == 1)
+
                 {
-                    Console.WriteLine(b + " is a factor of " + a);
+
+                    heads++;
+
+                }
+
+                else
+
+                {
+
+                    tails++;
+
+                }
+
+                double coin = rand.NextDouble();
+                if (coin < 0.5)
+                {
+                    Console.WriteLine(coin + " Tail");
+                    heads += 1;
+                }
+                else
+                {
+                    Console.WriteLine(coin + " Head");
+                    tails += 1;
                 }
             }
+            Console.WriteLine("The Ratio of Tail:Head is : ({0}:{1})", heads, tails);
+            Console.WriteLine("Percentage of Tail is : {0}%", tails * 10);
+            Console.WriteLine("Percentage of Head is : {0}%", heads * 10);
         }
     }
 }
